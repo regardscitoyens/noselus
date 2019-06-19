92ae1a5c4b8d4671053e35f3cbfecb530df7fc17
tarNeFyS <tarnefys@gmail.com>
2019-06-19 22:45:25 +0200

    Add all files in gather and add the basic treatment

--- noselus/ingest/src/gather-from-rne-history.sh
+++ noselus/ingest/src/gather-from-rne-history.sh
@@ -3,6 +3,38 @@ set -eu
 
 cd $(dirname $0)
 echo $(pwd)
+mkdir -p ../../../data/raw
 cd ../../../data
 
-wget https://github.com/regardscitoyens/rne-history/raw/master/1-rne-cm.txt
+
+gather_one_file () {
+  fname=$1	
+
+  echo "Treating the file : $fname" 
+
+  cd raw
+
+  wget https://github.com/regardscitoyens/rne-history/raw/master/${fname} 
+
+  cd ..
+
+  #Convert to utf8
+  iconv -f ISO-8859-15 -t UTF-8 raw/$fname > $fname
+
+  #Remove first line // Title
+  sed -i '1d' $fname
+
+  echo "$fname has been treated"
+}
+
+#gather_one_file "8-rne-deputes.txt"
+#files=("1-rne-cm.txt" "2-rne-epci.txt" "3-rne-cd.txt" "4-rne-cr.txt" "5-rne-rpe.txt" "7-rne-senateurs.txt" "8-rne-deputes.txt" "9-rne-maires.txt")
+files=("2-rne-epci.txt" "3-rne-cd.txt" "4-rne-cr.txt" "5-rne-cac.txt" "6-rne-rpe.txt" "7-rne-senateurs.txt" "8-rne-deputes.txt" "9-rne-maires.txt")
+
+
+for f in "${files[@]}" 
+do
+  gather_one_file $f
+done
+
+
