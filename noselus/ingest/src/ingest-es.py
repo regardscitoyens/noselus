0b5427d7fc9b3742dd7e16aedfffbfdb728043ec
tarNeFyS <tarnefys@gmail.com>
2019-06-19 23:53:57 +0200

    Ingest all the files into the ES

--- noselus/ingest/src/ingest-es.py
+++ noselus/ingest/src/ingest-es.py
@@ -6,63 +6,44 @@ from datetime import datetime
 
 es = Elasticsearch(['localhost'])
 
-with open('../../../data/1-rne-cm.txt') as maire_file:
-    reader = csv.reader(maire_file, delimiter='\t')
 
-    header = []
-    idx_count = 0
+def ingest_one_file(filename, mandat_name):
+    path = "../../../data/" +filename 
+    with open(path) as file:
+        reader = csv.reader(file, delimiter='\t')
 
-    for row in reader:
-        #Fill header for first row
-        if len(header) <= 0 :
-            for field in row:
-                header.append(field)
-        else :
-            doc = dict()
-            pos = 0
-            for field in row:
-                doc[header[pos]] = field
-                pos = pos +1
+        header = []
+        idx_count = 0
 
-            idx_count = idx_count+1
-            res = es.index(index="mandat-idx", id=idx_count, body=doc)
+        for row in reader:
+            #Fill header for first row
+            if len(header) <= 0 :
+                for field in row:
+                    header.append(field)
+            else :
+                doc = dict()
+                doc['mandat'] = mandat_name
 
-            #print(res)
-            if (idx_count%1000 == 0):
-                print(idx_count)
+                pos = 0
+                for field in row:
+                    doc[header[pos]] = field
+                    pos = pos +1
 
+                idx_count = idx_count+1
+                idx_str = "{}-{}".format(mandat_name,idx_count)
+                res = es.index(index="mandat-idx", id=idx_count, body=doc)
 
-
-exit()
-SystemExit()
-
-
-
-
-es = Elasticsearch(['localhost'])
-
-doc = {
-    'author': 'kimchy',
-    'text': 'Elasticsearch: cool. bonsai cool.',
-    'timestamp': datetime.now(),
-}
-
-print(type(doc))
-
-res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-print(res['result'])
-
-res = es.get(index="test-index", doc_type='tweet', id=1)
-print(res['_source'])
-
-es.indices.refresh(index="test-index")
-
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total']['value'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+                #print(res)
+                if (idx_count%1000 == 0):
+                    print("{} : {}".format(filename, idx_str))
 
 
-
-
-
+ingest_one_file("1-rne-cm.txt", "cm")
+ingest_one_file("2-rne-epci.txt", "epci")
+ingest_one_file('3-rne-cd.txt', "cd")
+ingest_one_file("4-rne-cr.txt", "cr")
+ingest_one_file("5-rne-cac.txt", "cac")
+ingest_one_file("6-rne-rpe.txt", "rpe")
+ingest_one_file("7-rne-senateurs.txt", "senateurs")
+ingest_one_file("8-rne-deputes.txt", "deputes")
+ingest_one_file("9-rne-maires.txt", "maires")
