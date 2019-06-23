911059a44b2759cc3bd0fe5586fb36688a65cc7c
Tangui Morlier <tangui@tangui.eu.org>
2019-06-23 18:16:13 +0200

    Use our own field name to have homogeneious ones

--- noselus/ingest/src/ingest-es.py
+++ noselus/ingest/src/ingest-es.py
@@ -7,24 +7,24 @@ from datetime import datetime
 
 es = Elasticsearch(['localhost'])
 
-
-def ingest_one_file(filename, mandat_name):
+def ingest_one_file(filename, mandat_name, header):
     path = "../../../data/" +filename
     with open(path) as file:
         reader = csv.reader(file, delimiter='\t')
 
-        header = []
         dates = []
-        idx_count = 0
+        idx_count = -1
+
+        for row in header:
+            if (re.search('Date', row)):
+                dates.append(row)
 
         for row in reader:
-            #Fill header for first row
-            if len(header) <= 0 :
-                for field in row:
-                    header.append(field)
-                    if (re.search('Date', field)):
-                        dates.append(field)
-            else :
+            #ignore the header line
+            if (idx_count < 0 ):
+                idx_count = 0;
+            else:
+                #Fill header for first row
                 doc = dict()
                 doc['mandat'] = mandat_name
                 pos = 0
@@ -44,12 +44,12 @@ def ingest_one_file(filename, mandat_name):
                     print("{} : {}".format(filename, idx_str))
 
 
-ingest_one_file("1-rne-cm.txt", "cm")
-ingest_one_file("2-rne-epci.txt", "epci")
-ingest_one_file('3-rne-cd.txt', "cd")
-ingest_one_file("4-rne-cr.txt", "cr")
-ingest_one_file("5-rne-cac.txt", "cac")
-ingest_one_file("6-rne-rpe.txt", "rpe")
-ingest_one_file("7-rne-senateurs.txt", "senateurs")
-ingest_one_file("8-rne-deputes.txt", "deputes")
-ingest_one_file("9-rne-maires.txt", "maires")
+ingest_one_file("1-rne-cm.txt", "cm", ["Code du département", "Libellé du département", "Code Insee de la commune", "Libellé de la commune", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat", "Libellé de fonction", "Date de début de la fonction", "Nationalité de l'élu"])
+ingest_one_file("2-rne-epci.txt", "epci", ["Code du département de l'EPCI", "N° SIREN", "Libellé de l'EPCI", "Code Insee de la commune", "Code du département", "Libellé de la commune", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat", "Libellé de fonction", "Date de début de la fonction"])
+ingest_one_file('3-rne-cd.txt', "cd", ["Code du département", "Libellé du département", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Code du canton", "Libellé du canton", "Libellé de fonction", "Code profession", "Libellé de la profession", "Date de naissance", "Date de début du mandat", "Date de début de la fonction"])
+ingest_one_file("4-rne-cr.txt", "cr", ["Code région", "Libellé de la région", "Code du département", "Libellé du département", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat", "Libellé de fonction", "Date de début de la fonction"])
+ingest_one_file("5-rne-cac.txt", "cac", ["Code région", "Libellé de la région", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat", "Libellé de fonction", "Date de début de la fonction"])
+# ingest_one_file("6-rne-rpe.txt", "rpe", ["Code CirER", "Libellé CirER", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat"])
+ingest_one_file("7-rne-senateurs.txt", "senateurs", ["Code du département", "Libellé du département", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat"])
+ingest_one_file("8-rne-deputes.txt", "deputes", ["Code du département", "Libellé du département", "Code de la circonscription législative", "Libellé de la circonscription législative", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat"])
+ingest_one_file("9-rne-maires.txt", "maires", ["Code du département", "Libellé du département", "Code Insee de la commune", "Libellé de la commune", "Nom de l'élu", "Prénom de l'élu", "Code sexe", "Date de naissance", "Code profession", "Libellé de la profession", "Date de début du mandat", "Date de début de la fonction"])
